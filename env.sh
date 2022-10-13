82a6c6be3b53d4200a2f285f0a0fe583ab5974b4
Avery Zhou <AveryZhou@users.noreply.github.com>
2022-10-12 22:34:27 -0500

    First

---
+++ env.sh
@@ -0,0 +1,4 @@
+#eval "$(/share/data/pals/avery/dreamfield_project/torch-dreamfield/mc3_tdf/bin/conda 'shell.bash' 'hook')" && . /share/data/pals/avery/dreamfield_project/torch-dreamfield/df-env/bin/activate
+#eval "$(/share/data/vision-greg/sudarshan/miniconda/bin/conda shell.bash hook)"
+#conda activate df_embed_run
+eval "$(/share/data/pals/sudarshan/final_mini/bin/conda shell.bash hook)"
